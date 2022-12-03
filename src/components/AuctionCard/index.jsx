b829c63101ba7b24658a5b7414b82c8cba09aa23
Jalil <benharkatdjalil@gmail.com>
2022-12-03 20:55:24 +0400

    first commit

--- src/components/AuctionCard/index.jsx
+++ src/components/AuctionCard/index.jsx
@@ -1,8 +1,68 @@
 import "./index.scss";
 import picassoFaceArt from "../../assets/picasso-face-art.jpeg";
-import { ConfigProvider, Button, Input, Space, Divider } from "antd";
+import { ConfigProvider, Button, Input, Space, Divider, message } from "antd";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function AuctionCard() {
+	const [subscriberEmail, setSubscriberEmail] = useState("");
+	const [messageApi, contextHolder] = message.useMessage();
+
+	const updateSubscriber = (event) => {
+		let { value } = event.target;
+		setSubscriberEmail(value);
+	};
+
+	const validateEmail = (email) => {
+		return String(email)
+			.toLowerCase()
+			.match(/^\S+@\S+\.\S+$/);
+	};
+
+	const handleSubscribe = () => {
+		if (!validateEmail(subscriberEmail)) {
+			messageApi.open({
+				type: "error",
+				content: "Invalid email address provided",
+			});
+			return;
+		}
+		messageApi.open({
+			type: "loading",
+			content: "Please wait...",
+			duration: 1,
+		});
+		console.log("aaaaa");
+		axios
+			.post(import.meta.env.VITE_BASE_URL + `/api/art/subscribe/`, {
+				artId: 1,
+				email: subscriberEmail,
+			})
+			.then((response) => {
+				if (response.status === 200) {
+					messageApi.open({
+						type: "success",
+						content: "You have subscribed successfully!",
+						duration: 5,
+					});
+				} else {
+					messageApi.open({
+						type: "error",
+						content: "Sorry we could not subscribe you, please try again later",
+						duration: 5,
+					});
+				}
+			})
+			.catch(() => {
+				messageApi.open({
+					type: "error",
+					content: "Sorry we could not subscribe you, please try again later",
+					duration: 5,
+				});
+			});
+		console.log("bbbbbb");
+	};
+
 	return (
 		<ConfigProvider
 			theme={{
@@ -11,6 +71,7 @@ function AuctionCard() {
 				},
 			}}
 		>
+			{contextHolder}
 			<div className="auction-card-container">
 				<div className="auction-image-container">
 					<img className="auction-image" src={picassoFaceArt} />
@@ -58,9 +119,16 @@ function AuctionCard() {
 								<Input
 									style={{ width: "calc(100% - 100px)" }}
 									placeholder="Email"
+									type="email"
 									className="subscribe-to-art-input"
+									onChange={(event) => updateSubscriber(event)}
+									value={subscriberEmail}
 								/>
-								<Button type="primary" className="subscribe-to-art-btn">
+								<Button
+									type="primary"
+									className="subscribe-to-art-btn"
+									onClick={handleSubscribe}
+								>
 									Subscribe
 								</Button>
 							</div>
