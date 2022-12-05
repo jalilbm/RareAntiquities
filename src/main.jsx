b72981cd32edd055ef2bdcd2aa527ebe38826c06
Jalil <benharkatdjalil@gmail.com>
2022-12-05 13:10:57 +0400

    added connect wallet

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Router>
+		<Router basename={"https://therareantiquities.netlify.app/"}>
 			<App />
 		</Router>
 	</React.StrictMode>
